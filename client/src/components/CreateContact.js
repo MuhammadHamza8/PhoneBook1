3375c2a394c100e3ef0846e1ddf013ff559c0acf
MuhammadHamza8 <hamzaicp54@gmail.com>
2025-04-03 23:06:33 +0500

    api rquest from frontend

--- client/src/components/CreateContact.js
+++ client/src/components/CreateContact.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios'
+
+
+
+
+
+
 
 const CreateContact = () => {
+
+
+
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState(0)
+  
+  const addNewNumber = () => {
+    Axios.post('http://localhost:8080/api/add-phone',{name,phone})
+  }
+
+
   return (
 
     <div className="grid-container">
      <a className='button'  href="/view">view contacts </a>
     <form action="/action_page.php"/>
     <label for="fname">Name </label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+    <input type="text" onChange={(e) => {setName(e.target.value)}}/>
 
     <label for="lname">Phone number</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your Number"/>
+    <input type="text" onChange={(e) => {setPhone(e.target.value)}}/><br/><br/>
 
-    <button className='addnumber'>Add New Number</button>
+
+    <button onClick={addNewNumber}  className='addnumber'>Add New Number</button>
   <form/>
 
 
--- client/src/components/ViewContact.js
+++ client/src/components/ViewContact.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect , useState} from 'react';
+import Axios from 'axios'
+
+
+
 
 
 const ViewContact=()=>{
 
+
+  const [phonebook, setPhonebook] = useState([])
+
+  useEffect(() => {
+    Axios.get('http://localhost:8080/api/get-phone').then(res => {
+      setPhonebook(res.data.data.phoneNumbers)
+    })
+  },[])
+
+
+
+
+  // delete Phone number 
+
+  const deletePhone = (id) => {
+    Axios.delete(`http://localhost:8080/api/delete-phone/${id}`)
+  }
+
+
 return(
+
+  
 
 <div className="grid-container">
 
-<h2>Contact List</h2>
+<h1>PhoneBook List</h1>
+
 
 <table>
   <tr>
@@ -15,18 +41,32 @@ return(
     <th>Phone Number</th>
     <th>Action</th>
   </tr>
-  <tr>
-    <td>Peter</td>
-    <td>Griffin</td>
-    <td>
-      
-      <button class="button button3">Delete</button>
 
-      <button  class="button button3">Update</button>
+  {
     
-    
-    </td>
-  </tr>
+
+        phonebook.map((val,key) => {
+          return  <tr>
+          <td>{val.name}</td>
+          <td>{val.phone}</td>
+      
+     
+          <td>
+            
+            <button class="button button3">Update</button>
+      
+            <button onClick={() =>{deletePhone(val._id)}} class="button button3">Delete</button>
+          
+          
+          </td>
+        </tr>
+
+        })
+
+        
+      }
+
+ 
 
 </table>
 
